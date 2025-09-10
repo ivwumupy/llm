73b8713b986fa5d10a27f58df5d0b3064ebd20ce
parabola2004 <chengzhuo2004@qq.com>
2025-09-10 22:24:59 +0800

    fix: weighted data source member init

--- spargel_llm/data_source.py
+++ spargel_llm/data_source.py
@@ -25,8 +25,18 @@ class PlainTextSource(DataSource[str]):
     _random: Random
 
     def __init__(
-        self, text: str, min_len: int, max_len: int, *, random: Random = Random()
+        self, text: str, min_len: int, max_len: int | None, *, random: Random = Random()
     ):
+        """
+        Args:
+            text (str): the text to sample from
+            min_len (int): minimum length for sampled text
+            max_len (int | None): maximum length for sampled text; equal to min_len if not provided
+        """
+
+        if max_len is None:
+            max_len = min_len
+
         assert min_len >= 0 and max_len >= min_len and max_len <= len(text)
 
         self._text = text
@@ -35,12 +45,16 @@ class PlainTextSource(DataSource[str]):
 
     @override
     def sample(self) -> str:
-        length = self._random.randint(self._min_len, self._max_len)
+        if self._min_len == self._max_len:
+            length = self._random.randint(self._min_len, self._max_len)
+        else:
+            length = self._min_len
+
         start = self._random.randint(0, len(self._text) - length)
         return self._text[start : start + length]
 
 
-class GeneratedSource[T](DataSource[T]):
+class GeneratedDataSource[T](DataSource[T]):
     """Data source that samples by calling a generator function.
 
     The generator function can use a Random instance to probuce random results.
@@ -58,14 +72,14 @@ class GeneratedSource[T](DataSource[T]):
         return self._func(self._random)
 
 
-class WeightedSource[T](DataSource[T]):
+class WeightedDataSource[T](DataSource[T]):
     """Data source that samples from multiple sources randomly.
 
     Each time, one of the sources is randomly chosen according to the provided weights.
     """
 
-    _weights: list[float] = []
-    _sources: list[DataSource[T]] = []
+    _weights: list[float]
+    _sources: list[DataSource[T]]
     _random: Random
 
     def __init__(
@@ -74,6 +88,9 @@ class WeightedSource[T](DataSource[T]):
         *,
         random: Random = Random(),
     ):
+        self._weights = []
+        self._sources = []
+
         sum_of_weights = 0.0
         for weight, source in sources:
             assert weight >= 0.0
--- tests/test_data_source.py
+++ tests/test_data_source.py
@@ -3,23 +3,42 @@ import time
 import unittest
 from random import Random
 
-from spargel_llm.data_source import PlainTextSource, GeneratedSource, WeightedSource
+from spargel_llm.data_source import (
+    PlainTextSource,
+    GeneratedDataSource,
+    WeightedDataSource,
+)
 
 seed = time.time()
 # print("seed:", seed)
 
 
 class TestPlainTextSource(unittest.TestCase):
+    def test_fixed_length(self):
+        random = Random(seed)
+
+        text_length = random.randint(16, 1000000)
+        text = "".join(
+            random.choices(string.ascii_letters + string.digits, k=text_length)
+        )
+
+        sample_length = random.randint(1, text_length // 16)
+        source = PlainTextSource(text, sample_length, sample_length, random=random)
+        for _ in range(100):
+            sampled_text = source.sample()
+            self.assertTrue(text.find(sampled_text) >= 0)
+            self.assertEqual(len(sampled_text), sample_length)
+
     def test(self):
         random = Random(seed)
 
-        text_length = random.randint(1, 1000000)
+        text_length = random.randint(16, 1000000)
         text = "".join(
             random.choices(string.ascii_letters + string.digits, k=text_length)
         )
 
         source = PlainTextSource(text, 1, text_length // 16, random=random)
-        for _ in range(1000):
+        for _ in range(100):
             sampled_text = source.sample()
             self.assertTrue(text.find(sampled_text) >= 0)
 
@@ -29,14 +48,14 @@ class TestGeneratedSource(unittest.TestCase):
         random = Random(seed)
 
         n = random.randint(1, 100)
-        source = GeneratedSource(lambda _: n, random=random)
+        source = GeneratedDataSource(lambda _: n, random=random)
         for _ in range(100):
             self.assertEqual(source.sample(), n)
 
     def test(self):
         random = Random(seed)
 
-        source = GeneratedSource(lambda r: r.randint(95, 105), random=random)
+        source = GeneratedDataSource(lambda r: r.randint(95, 105), random=random)
         for _ in range(100):
             x = source.sample()
             self.assertTrue(95 <= x and x <= 105)
@@ -47,19 +66,18 @@ class TestWeightedSource(unittest.TestCase):
         random = Random(seed)
 
         n = random.randint(1, 100)
-        source = WeightedSource([(1, GeneratedSource(lambda _: n))])
+        source = WeightedDataSource([(1, GeneratedDataSource(lambda _: n))])
         for _ in range(100):
             self.assertEqual(source.sample(), n)
 
     def test_two(self):
         random = Random(seed)
 
-        a = random.randint(1, 100)
-        b = random.randint(1, 100)
-        source = WeightedSource(
+        a, b = random.randint(1, 100), random.randint(1, 100)
+        source = WeightedDataSource(
             [
-                (1, GeneratedSource(lambda _: a)),
-                (2, GeneratedSource(lambda _: b)),
+                (1, GeneratedDataSource(lambda _: a)),
+                (2, GeneratedDataSource(lambda _: b)),
             ]
         )
         for _ in range(100):
